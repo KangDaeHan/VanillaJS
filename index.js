1f5d04742caa416faa78c3621d98b41440241ecd
kdh7565 <kdh7565@sk.com>
2019-04-30 16:53:56 +0900

    시간 수정

--- index.js
+++ index.js
@@ -1,77 +1,78 @@
 const clockContainer = document.querySelector(".js_clock"),
-	clockTitle = clockContainer.querySelector(".js_title");
+    clockTitle = clockContainer.querySelector(".js_title");
 
 const form = document.querySelector(".js_form"),
-	input = form.querySelector("input"),
-	greeting = document.querySelector(".greeting");
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".greeting");
 
 const USER_LS = "currentUser",
-	SHOW_CH = "showing";
+    SHOW_CH = "showing";
 
 const body = document.querySelector("body");
 
 const IMG_NUMBER = 3;
 
 function paintImage(imgNumber) {
-	const image = new Image();
-	image.src = `images/${imgNumber + 1}.jpg`;
-	image.classList.add("bg_image");
-	body.appendChild(image);
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`;
+    image.classList.add("bg_image");
+    body.appendChild(image);
 }
 
 function genRandom() {
-	const number = Math.floor(Math.random() * IMG_NUMBER);
-	return number;
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
 }
 
 function getTime() {
-	const date = new Date();
-	const minutes = date.getMinutes();
-	const hours = date.getHours() > 12 ? `오후 0${date.getHours() - 12}` : `오전 ${date.getHours()}`;
-	const seconds = date.getSeconds();
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const ampm = hours >= 12 ? "오후" : "오전";
+    const ch_hours = hours % 12;
+    const write_hours = ch_hours ? ch_hours : 12;
 
-	clockTitle.innerHTML = `${hours < 10 ? hours : hours}:${
-		minutes < 10 ? `0${minutes}` : minutes
-		}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    clockTitle.innerHTML = `${ampm} ${write_hours < 10 ? `0${write_hours}` : write_hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function saveName(text) {
-	localStorage.setItem(USER_LS, text);
+    localStorage.setItem(USER_LS, text);
 }
 
 function handleSubmit(e) {
-	e.preventDefault();
-	const currentValue = input.value;
-	console.log(currentValue);
-	paintGreeting(currentValue);
-	saveName(currentValue);
+    e.preventDefault();
+    const currentValue = input.value;
+    console.log(currentValue);
+    paintGreeting(currentValue);
+    saveName(currentValue);
 }
 
 function askName() {
-	form.classList.add(SHOW_CH);
-	form.addEventListener("submit", handleSubmit);
+    form.classList.add(SHOW_CH);
+    form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
-	form.classList.remove(SHOW_CH);
-	greeting.classList.add(SHOW_CH);
-	greeting.innerHTML = `hello ${text}`;
+    form.classList.remove(SHOW_CH);
+    greeting.classList.add(SHOW_CH);
+    greeting.innerHTML = `hello ${text}`;
 }
 
 function loadName() {
-	const currentUser = localStorage.getItem(USER_LS);
-	if (currentUser === null) {
-		askName();
-	} else {
-		paintGreeting(currentUser);
-	}
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+        askName();
+    } else {
+        paintGreeting(currentUser);
+    }
 }
 
 function init() {
-	setInterval(getTime, 1000);
-	loadName();
-	const randomNumber = genRandom();
-	paintImage(randomNumber);
+    setInterval(getTime, 1000);
+    loadName();
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
 }
 
 init();
